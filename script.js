00de5f7396dc15e6e68946f355818780aef5d9ee
Deyvisson-del <deyvissonrafael018@gmail.com>
2024-12-10 16:32:51 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Digite o nome do seu jogador:");
+var nomedojogador = "Deyvisson";
+
+console.log("Bem vindo " + nomedojogador);
+
+let name = prompt("Digite o nome do seu personagem:");
+
+// var number1 = 1;
+// var number2 = 1;
+// console.log(number1 + number2);
